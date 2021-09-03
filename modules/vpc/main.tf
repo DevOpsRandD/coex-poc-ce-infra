1eec259a0844671012995199ff4ff198c3ddf347
Juan Ocampo <Juan.Ocampo@gmfinancial.com>
2021-09-03 13:24:54 -0500

    Added route tables and route table associations

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,6 +6,9 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+
+## Private Subnets
+
 resource "aws_subnet" "priv_1" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = var.sub_priv_1_cidr
@@ -35,6 +38,9 @@ resource "aws_subnet" "priv_3" {
     Project = var.project
   }
 }
+
+
+## Public Subnets
 
 resource "aws_subnet" "pub_1" {
   vpc_id            = aws_vpc.vpc.id
@@ -66,12 +72,75 @@ resource "aws_subnet" "pub_3" {
   }
 }
 
+
+
+## Internet Gateway
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
   tags = {
     Name    = format("%s-igw", var.vpc_name)
     Project = var.project
   }
+}
+
+
+## Route Tables
+
+resource "aws_route_table" "priv-nodes-rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = format("%s-priv-nodes", var.vpc_name)
+    Project = var.project
+  }
+}
+
+
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = format("%s-public", var.vpc_name)
+    Project = var.project
+  }
+}
+
+
+
+## Route table associations
+
+### Private Routes
+resource "aws_route_table_association" "priv-1-subnet-association" {
+  subnet_id      = aws_subnet.priv_1.id
+  route_table_id = aws_route_table.priv-nodes-rt.id
+}
+
+resource "aws_route_table_association" "priv-2-subnet-association" {
+  subnet_id      = aws_subnet.priv_2.id
+  route_table_id = aws_route_table.priv-nodes-rt.id
+}
+
+resource "aws_route_table_association" "priv-3-subnet-association" {
+  subnet_id      = aws_subnet.priv_3.id
+  route_table_id = aws_route_table.priv-nodes-rt.id
+}
+
+
+### Public Routes
+resource "aws_route_table_association" "pub-1-subnet-association" {
+  subnet_id      = aws_subnet.pub_1.id
+  route_table_id = aws_route_table.pub-nodes-rt.id
+}
+
+resource "aws_route_table_association" "pub-2-subnet-association" {
+  subnet_id      = aws_subnet.pub_2.id
+  route_table_id = aws_route_table.pub-nodes-rt.id
+}
+
+resource "aws_route_table_association" "pub-3-subnet-association" {
+  subnet_id      = aws_subnet.pub_3.id
+  route_table_id = aws_route_table.pub-nodes-rt.id
 }
 
 
