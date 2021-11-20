3ac0323163ce84174b545987e91251b68bdb17b5
Alejandro Ochoa M <alejandro.ochoam@perficient.com>
2021-11-20 09:00:01 -0500

    Add missing module

--- main.tf
+++ main.tf
@@ -6,6 +6,11 @@ module "chaos_engineering_network" {
   config   = var.vpc_config
 }
 
+# IAM Role
+module "eks_iam" {
+  source = "./modules/eks_iam"
+}
+
 resource "aws_ecr_repository" "repositories" {
   for_each = toset(var.ecr_repos)
   name                 = format("rand-%s", each.value)
